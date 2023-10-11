0f5faafb4fee4956d9603513f30e1018af2c0f74
Bogdan Filip <bogdan.filip@tecknoworks.com>
2023-10-11 15:12:39 +0300

    Modified code: solved errors

--- collaborative_practice.js
+++ collaborative_practice.js
@@ -9,7 +9,7 @@ const printMenu = () => {
     Select an option:
         1. List all users
         2. Create new user
-		5. Search user in list of users
+        5. Search by first name or last name
         0. Exit
     `);
 };
@@ -22,18 +22,32 @@ const listUsers = (users) => {
     console.log(output);
 };
 
-const searchUsers = (firstName, lastName, users) => {
-    const output = users
-        .map(user => (user.firstName === firstName && user.lastName === lastName) 
-        ? `ID: ${user.id}, First name: ${user.firstName}, Last name: ${user.lastName}`
-        : "User not found")
-    
-    console.log(output)
+const searchUsers = (name, users) => {
+    if (name.length >= 2) {
+        let exists = false;
+        users
+            .forEach(user => {
+                if (user.firstName.toLowerCase() === name.toLowerCase() || user.lastName.toLowerCase() === name.toLowerCase()) {
+                    console.log(`ID: ${user.id}, First name: ${user.firstName}, Last name: ${user.lastName}\n`)
+                    exists = true;
+                }
+            })
+            
+        if (!exists) {
+            console.log("The name does not exist!")
+        }
+    }
+    else {
+        console.log("Name length too short!")
+    }
+
 }
 
 const runProgram = async () => {
     let users = [
-        { id: 1, firstName: "John", lastName: "Smith" }
+        { id: 1, firstName: "Dan", lastName: "Smith" },
+        { id: 2, firstName: "John", lastName: "Daniels" },
+        { id: 3, firstName: "Marcel", lastName: "John" }
     ];
     while (true) {
         try {
@@ -46,21 +60,20 @@ const runProgram = async () => {
                     break;
 
                 case "2":
-                    const newUser={
-                        id:users.length+1,
-                        firstName:await prompt("FirstName:"),
-                        lastName:await prompt("Lastname:")
+                    const newUser = {
+                        id: users.length + 1,
+                        firstName: await prompt("FirstName:"),
+                        lastName: await prompt("Lastname:")
                     }
-                    if(newUser.firstName===null || newUser.lastName===null){
+                    if (newUser.firstName === null || newUser.lastName === null) {
                         console.log("ceva");
                     }
-                    
+
                     users.push(newUser)
                     console.log("User createad succesfully!");
                     break;
                 case "5":
-                    console.log("Person you are looking for: ")
-                    searchUsers(await prompt("First Name: "), await prompt("Last Name: "), users)
+                    searchUsers(await prompt("Please enter a first name or a last name: "), users)
                     break;
                 case "0":
                     rl.close();
