dfb6f72dede39f94b7268aa2f0147d7ffd992d1a
Marco Munteanu <marco.munteanu@tecknoworks.com>
2023-10-14 22:09:42 +0300

    implement business layer operations

---
+++ DemoApp/DemoApp/BusinessLayer/Services/IStudentsService.cs
@@ -0,0 +1,14 @@
+﻿
+using BusinessLayer.Models;
+
+namespace BusinessLayer.Services
+{
+    public interface IStudentsService
+    {
+        Task<IEnumerable<Student>> GetStudentsAsync();
+        Task<Student?> GetStudentAsync(int studentId);
+        Task<bool> StudentExistsAsync(int studentId);
+        void CreateStudent(Student newStudent);
+        void DeleteStudent(int studentId);
+    }
+}
---
+++ DemoApp/DemoApp/BusinessLayer/Services/StudentsService.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Models;
+using DataAccessLayer.Repositories;
+
+namespace BusinessLayer.Services
+{
+    public class StudentsService : IStudentsService
+    {
+        private readonly IStudentsRepository _studentsRepository;
+       
+        public StudentsService(IStudentsRepository studentsRepository)
+        { 
+            _studentsRepository = studentsRepository ?? throw new ArgumentNullException(nameof(studentsRepository));
+        }
+
+        public async Task<IEnumerable<Student>> GetStudentsAsync()
+        {
+            return await _studentsRepository.GetStudentsAsync();
+
+        }
+
+        public async Task<Student?> GetStudentAsync(int studentId)
+        {
+            return await _studentsRepository.GetStudentAsync(studentId);
+        }
+
+        public async Task<bool> StudentExistsAsync(int studentId)
+        {
+            return await _studentsRepository.StudentExistsAsync(studentId);
+        }
+
+        public void CreateStudent(Student newStudent)
+        {
+            _studentsRepository.CreateStudent(newStudent);
+        }
+
+        public void DeleteStudent(int studentId)
+        {
+            _studentsRepository.DeleteStudent(studentId);
+        }
+    }
+}
