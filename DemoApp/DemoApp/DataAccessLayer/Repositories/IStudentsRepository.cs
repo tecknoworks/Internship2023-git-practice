db5cb1fd4bd5db5a2ba9b6c76ae586f5682efead
Marco Munteanu <marco.munteanu@tecknoworks.com>
2023-10-14 22:09:07 +0300

    implement data access layer operations

---
+++ DemoApp/DemoApp/DataAccessLayer/Repositories/IStudentsRepository.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLayer.Models;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IStudentsRepository
+    {
+        Task<IEnumerable<Student>> GetStudentsAsync();
+        Task<Student?> GetStudentAsync(int studentId);
+        Task<bool> StudentExistsAsync(int studentId);
+        void CreateStudent(Student newStudent);
+        void DeleteStudent(int studentId);
+    }
+}
---
+++ DemoApp/DemoApp/DataAccessLayer/Repositories/StudentsRepository.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Models;
+using System.Security.Cryptography;
+
+namespace DataAccessLayer.Repositories
+{
+    public class StudentsRepository : IStudentsRepository
+    {
+        private List<Student> _students = new List<Student>()
+        {
+            new Student() { Id = 101, Name = "James", Branch = "CSE", Section = "A", Gender = "Male" },
+            new Student() { Id = 102, Name = "Smith", Branch = "ETC", Section = "B", Gender = "Male" },
+            new Student() { Id = 103, Name = "David", Branch = "CSE", Section = "A", Gender = "Male" },
+            new Student() { Id = 104, Name = "Sara", Branch = "CSE", Section = "A", Gender = "Female" },
+            new Student() { Id = 105, Name = "Pam", Branch = "ETC", Section = "B", Gender = "Female" }
+        };
+
+        private async Task<IEnumerable<Student>> DataSourceAsync()
+        {
+            return await Task.FromResult(_students);
+        }
+
+        public async Task<IEnumerable<Student>> GetStudentsAsync()
+        {
+            return await DataSourceAsync();
+        }
+
+        public async Task<Student?> GetStudentAsync(int studentId)
+        {
+            var students = await DataSourceAsync();
+            return students.FirstOrDefault(student => student.Id == studentId);
+        }
+
+        public async Task<bool> StudentExistsAsync(int studentId)
+        {
+            var students = await DataSourceAsync();
+            return students.Any(student => student.Id == studentId);
+        }
+
+        public void CreateStudent(Student newStudent)
+        {
+            int maxId = _students.Max(student => student.Id);
+            newStudent.Id = maxId + 1;
+            _students.Add(newStudent);
+        }
+
+        public void DeleteStudent(int studentId)
+        {
+            _students.RemoveAll(student => student.Id == studentId);
+        }
+    }
+}
